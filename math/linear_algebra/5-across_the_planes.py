5dde2626f516e8206e2478d9a2f5260eb6ae1812
boughanmiyoussef <yussefboughanmy@gmail.com>
2025-04-04 15:23:56 +0100

    Pair Element With Rows

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
+"""
+adds two matrices element-wise
+"""
 
 
 def add_matrices2D(mat1, mat2):
     """
+    adds two matrices element-wise
     """
 
     if len(mat1) != len(mat2):
