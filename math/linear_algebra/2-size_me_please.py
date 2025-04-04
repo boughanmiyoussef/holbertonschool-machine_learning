50d1b0bf996b0eaeb3fe76c6ba53b79193d4b9cd
boughanmiyoussef <yussefboughanmy@gmail.com>
2025-04-04 11:41:25 +0100

    Size The Matrix

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+def matrix_shape(matrix):
+    """
+    Return the sape of matrix.
+    """
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0] if matrix else []
+    return shape
+
