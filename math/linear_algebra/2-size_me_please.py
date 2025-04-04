47bebaa6bfa235519e5b25dce5f86831e106cd46
boughanmiyoussef <yussefboughanmy@gmail.com>
2025-04-04 11:50:12 +0100

    Size The Matrix

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 
+"""
+This module provides a function to calculate the shape of a matrix.
+The shape is returned as a list of integers representing the dimensions.
+"""
+
 
 def matrix_shape(matrix):
     """
