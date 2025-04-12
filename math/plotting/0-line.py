5cd7efc3ddf4888be52cd3a14fd9dc2f3e60b2f2
boughanmiyoussef <yussefboughanmy@gmail.com>
2025-04-12 18:53:10 +0100

    line graph

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
+"""
+This module contains a function to plot a line graph of y = x^3.
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def line():
+    """
+    Plots y = x^3 as a solid red line.
+
+    The x-axis ranges from 0 to 10.
+    """
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
 
