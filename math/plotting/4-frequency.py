f19c8b7ab63e6a5b6c123489f60856a3f5c20530
boughanmiyoussef <yussefboughanmy@gmail.com>
2025-04-12 19:26:22 +0100

    commit

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,35 +1,17 @@
 #!/usr/bin/env python3
-"""Module to plot a histogram of student grades."""
-
 import numpy as np
 import matplotlib.pyplot as plt
 
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
 
-def frequency():
-    """Plot a histogram of student grades with bins every 10 units."""
-    np.random.seed(5)
-    student_grades = np.random.normal(68, 15, 50)
-    plt.figure(figsize=(6.4, 4.8))
-    
-    # Define bins for 0-10, 10-20, ..., 90-100
-    bins = np.arange(0, 101, 10)
-    
-    # Plot histogram with black-outlined bars
-    plt.hist(student_grades, bins=bins, edgecolor='black')
-    
-    # Set axis labels
-    plt.xlabel('Grades')
-    plt.ylabel('Number of Students')
-    
-    # Set title
-    plt.title('Project A')
-    
-    # Set x-axis range and ticks
-    plt.xlim(0, 100)
-    plt.xticks(np.arange(0, 101, 10))
-    
-    # Adjust layout to match default rendering
-    plt.tight_layout()
-    
-    # Display the plot
-    plt.show()
+plt.hist(student_grades,
+         bins=[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
+         edgecolor='black')
+plt.xlabel('Grades')
+plt.ylabel('Number of Students')
+plt.title("Project A")
+plt.xlim((0, 100))
+plt.xticks(np.arange(0, 101, 10))
+plt.ylim((0, 30))
+plt.show()
