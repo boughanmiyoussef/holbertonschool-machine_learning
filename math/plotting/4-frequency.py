f76b690b918469acdbfa3df708d2671a9bfc6249
boughanmiyoussef <yussefboughanmy@gmail.com>
2025-04-12 19:18:25 +0100

    commit

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,31 +1,28 @@
 #!/usr/bin/env python3
-"""Plotting a histogram of student grades for Project A."""
-
+"""Plot a histogram of student grades."""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-def main():
-    """
-    Generate random student grades and plot a histogram.
-
-    - Title: 'Project A'
-    - X-axis: 'Grades'
-    - Y-axis: 'Number of Students'
-    - Bins every 10 units
-    - Bars outlined in black
-    """
+def frequency():
+    """Plot histogram of student grades with black edges."""
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
-
-    bins = np.arange(0, 110, 10)
+    plt.figure(figsize=(6.4, 4.8))
+    
+    # Create bins from 0 to 100 in steps of 10
+    bins = np.arange(0, 101, 10)
+    
+    # Plot histogram with black edges
     plt.hist(student_grades, bins=bins, edgecolor='black')
-    plt.xlabel("Grades")
-    plt.ylabel("Number of Students")
-    plt.title("Project A")
-    plt.xticks(bins)
+    
+    # Set labels and title
+    plt.xlabel('Grades')
+    plt.ylabel('Number of Students')
+    plt.title('Project A')
+    
+    # Set x-axis limits to match bins
+    plt.xlim(0, 100)
+    
     plt.show()
-
-
-if __name__ == "__main__":
-    main()
+    
