fdb4a5168cb2a196882039824e1e79496b97b04d
boughanmiyoussef <yussefboughanmy@gmail.com>
2025-04-12 19:03:44 +0100

    scatter graph

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -1,25 +1,23 @@
 #!/usr/bin/env python3
-"""
-Plot a scatter plot of height vs weight with magenta points.
-"""
+"""Module to plot a scatter plot of height vs weight."""
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def scatter():
-    """
-    Scatter plot of height vs weight with magenta points.
-    """
+    """Plot a scatter plot of men's height vs weight with magenta points."""
     mean = [69, 0]
     cov = [[15, 8], [8, 15]]
     np.random.seed(5)
     x, y = np.random.multivariate_normal(mean, cov, 2000).T
     y += 180
-
     plt.figure(figsize=(6.4, 4.8))
-    plt.scatter(x, y, color='magenta', s=10)
-    plt.xlabel('Height (in)', fontsize='x-small')
-    plt.ylabel('Weight (lbs)', fontsize='x-small')
-    plt.title('Men\'s Height vs Weight', fontsize='x-small')
+
+    plt.scatter(x, y, c='magenta')
+
+    plt.xlabel('Height (in)')
+    plt.ylabel('Weight (lbs)')
+    plt.title("Men's Height vs Weight")
+
     plt.show()
