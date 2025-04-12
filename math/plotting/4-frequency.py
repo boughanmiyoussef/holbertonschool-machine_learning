6e3a617c3dd1ae91d8db5e5616eb621ee352e03d
boughanmiyoussef <yussefboughanmy@gmail.com>
2025-04-12 19:15:29 +0100

    commit

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
-"""Plot a histogram of student grades."""
+"""Plot a histogram showing frequency of student grades"""
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def frequency():
-    """Plot histogram of student grades with black edges."""
+    """Plots a histogram of student grades with bins of 10"""
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
-    plt.figure(figsize=(6.4, 4.8))
-    plt.hist(student_grades, bins=range(0, 101, 10), edgecolor='black')
-    plt.xlabel('Grades')
-    plt.ylabel('Number of Students')
-    plt.title('Project A')
+    bins = range(0, 101, 10)
+    plt.hist(student_grades, bins=bins, edgecolor='black')
+    plt.xlabel("Grades")
+    plt.ylabel("Number of Students")
+    plt.title("Project A")
+    plt.xticks(bins)
     plt.show()
-    
