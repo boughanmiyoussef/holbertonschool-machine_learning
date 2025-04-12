e3bbf03cf0aef5889c44bc1bdd4bac7ca9a77be8
boughanmiyoussef <yussefboughanmy@gmail.com>
2025-04-12 19:02:28 +0100

    scatter graph

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-This module contains a function to plot a scatter graph of height vs weight.
+Plot a scatter plot of height vs weight with magenta points.
 """
 
 import numpy as np
@@ -9,19 +9,17 @@ import matplotlib.pyplot as plt
 
 def scatter():
     """
-    Plots a scatter graph of men's height vs weight.
+    Scatter plot of height vs weight with magenta points.
     """
     mean = [69, 0]
     cov = [[15, 8], [8, 15]]
     np.random.seed(5)
     x, y = np.random.multivariate_normal(mean, cov, 2000).T
     y += 180
+
     plt.figure(figsize=(6.4, 4.8))
-
     plt.scatter(x, y, color='magenta', s=10)
-
     plt.xlabel('Height (in)', fontsize='x-small')
     plt.ylabel('Weight (lbs)', fontsize='x-small')
-    plt.title("Men's Height vs Weight", fontsize='x-small')
-
+    plt.title('Men\'s Height vs Weight', fontsize='x-small')
     plt.show()
