be8c3c5e85f3b55680ffadcfd588bc46d50cb399
boughanmiyoussef <yussefboughanmy@gmail.com>
2025-04-12 19:17:57 +0100

    commit

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -1,24 +1,23 @@
 #!/usr/bin/env python3
-"""Module that plots a histogram of student grades."""
+"""Plotting a histogram of student grades for Project A."""
 
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-def frequency():
+def main():
     """
-    Generate and plot a histogram of student grades for a project.
+    Generate random student grades and plot a histogram.
 
-    - The x-axis is labeled 'Grades'
-    - The y-axis is labeled 'Number of Students'
-    - The x-axis has bins every 10 units
-    - The bars are outlined in black
-    - The title is 'Project A'
+    - Title: 'Project A'
+    - X-axis: 'Grades'
+    - Y-axis: 'Number of Students'
+    - Bins every 10 units
+    - Bars outlined in black
     """
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
 
-    plt.figure(figsize=(6.4, 4.8))
     bins = np.arange(0, 110, 10)
     plt.hist(student_grades, bins=bins, edgecolor='black')
     plt.xlabel("Grades")
@@ -26,3 +25,7 @@ def frequency():
     plt.title("Project A")
     plt.xticks(bins)
     plt.show()
+
+
+if __name__ == "__main__":
+    main()
