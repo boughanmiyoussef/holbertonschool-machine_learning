52555c0ed6ce008196ceebe93b2b4c2212c8c32c
boughanmiyoussef <yussefboughanmy@gmail.com>
2025-04-12 19:33:01 +0100

    commit

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -5,13 +5,10 @@ import matplotlib.pyplot as plt
 np.random.seed(5)
 student_grades = np.random.normal(68, 15, 50)
 
-plt.hist(student_grades,
-         bins=[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100],
-         edgecolor='black')
-plt.xlabel('Grades')
-plt.ylabel('Number of Students')
+plt.xlabel("Grades")
+plt.ylabel("Number of Students")
 plt.title("Project A")
-plt.xlim((0, 100))
-plt.xticks(np.arange(0, 101, 10))
-plt.ylim((0, 30))
+plt.xlim(0, 100)
+plt.ylim(0, 30)
+plt.hist(student_grades, bins=10, range=(0, 100), rwidth=10, color='blue', edgecolor='black')
 plt.show()
