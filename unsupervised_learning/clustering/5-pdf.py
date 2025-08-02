f10663e1d63ed30358af5dbd046e347bfc25b7ec
Youssef <yussefboughanmy@gmail.com>
2025-08-02 21:58:00 +0100

    Clustering

--- unsupervised_learning/clustering/5-pdf.py
+++ unsupervised_learning/clustering/5-pdf.py
@@ -1,60 +1,34 @@
-
 #!/usr/bin/env python3
-"""PDF function """
+# 5-pdf.py
+"""5-pdf.py"""
+
 
 import numpy as np
 
 
 def pdf(X, m, S):
     """
-    Probability Density Function of gaussian distributions
-    Args:
-        X: numpy.ndarray of shape (n, d) containing the data points whose
-           PDF should be evaluated
-        m: numpy.ndarray of shape (d,) containing the mean of the distribution
-        S: numpy.ndarray of shape (d, d) containing the covariance of the
-           distribution
-    Returns: P, or None on failure
-             P: numpy.ndarray of shape (n,) containing the PDF values for each
-                data point.
-                All values in P should have a minimum value of 1e-300
+    calculates the probability density function of a Gaussian distribution
     """
-    if not isinstance(X, np.ndarray) or len(X.shape) != 2:
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
         return None
-    if not isinstance(m, np.ndarray) or len(m.shape) != 1:
+    if not isinstance(m, np.ndarray) or m.ndim != 1:
         return None
-    if not isinstance(S, np.ndarray) or len(S.shape) != 2:
+    if not isinstance(S, np.ndarray) or S.ndim != 2:
         return None
-    if X.shape[1] != m.shape[0] or X.shape[1] != S.shape[0]:
+    if (X.shape[1] != m.shape[0] or S.shape[0] != S.shape[1]
+            or S.shape[0] != m.shape[0]):
         return None
-    if S.shape[0] != S.shape[1]:
-        return None
 
-    # formula
-    # p(x∣ μ,Σ) = (1 √(2π)d|Σ|)exp(−1/2(x−μ)T Σ−1(x−μ))
-    n, d = X.shape
-    mean = m
-    x_m = X - mean
+    det_cov = np.linalg.det(S)
+    if det_cov <= 0:  # Check for invalid determinant
+        return np.full(X.shape[0], 1e-300)
 
-    # Determinant of the covariance matrix (d x d)
-    det_S = np.linalg.det(S)
+    inv_cov = np.linalg.inv(S)
+    X_centered = X - m
+    coef = 1 / np.sqrt((2 * np.pi) ** X.shape[1] * det_cov)
+    exponent = -0.5 * np.sum(X_centered @ inv_cov * X_centered, axis=1)
 
-    # Since Σ is Hermitian, it has an eigendecomposition
-    inv_S = np.linalg.inv(S)
-
-    # Formula Section one: (1 √(2π)d|Σ|)
-    part_1_dem = np.sqrt(det_S) * ((2 * np.pi) ** (d/2))
-
-    # Formula Section two_upper_1: −1/2(x−μ)T
-    part_2 = np.matmul(x_m, inv_S)
-
-    # Formula Section two_upper_2: Σ−1(x−μ) used diagonal to fix alloc err
-    part_2_1 = np.sum(x_m * part_2, axis=1)
-
-    # Formula Section two exp(−1/2(x−μ)T Σ−1(x−μ))
-    part_2_2 = np.exp(part_2_1 / -2)
-
-    # pdf = part_1 * part_2_2:
-    pdf = part_2_2 / part_1_dem
-    P = np.where(pdf < 1e-300, 1e-300, pdf)
+    P = coef * np.exp(exponent)
+    P = np.maximum(P, 1e-300)
     return P
