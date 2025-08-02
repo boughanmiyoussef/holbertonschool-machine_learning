4ae1971d9db2dd704ae07d8dd314c1eb0e562dc7
Youssef <yussefboughanmy@gmail.com>
2025-08-02 22:02:10 +0100

    Clustering

--- unsupervised_learning/clustering/4-initialize.py
+++ unsupervised_learning/clustering/4-initialize.py
@@ -1,39 +1,35 @@
 #!/usr/bin/env python3
-"""GMM function """
-
+"""
+Clustering module
+"""
 import numpy as np
 kmeans = __import__('1-kmeans').kmeans
 
 
 def initialize(X, k):
     """
-    initializes variables for a Gaussian Mixture Model
+    Initializes variables for a Gaussian Mixture Model
     Args:
-        X: numpy.ndarray of shape (n, d) containing the data set
-        k: positive integer containing the number of clusters
-    Returns: pi, m, S, or None, None, None on failure
-            pi: numpy.ndarray of shape (k,) containing the priors
-                for each cluster initialized evenly
-            m: numpy.ndarray of shape (k, d) containing the centroid
-                means for each cluster, initialized with K-means
-            s: numpy.ndarray of shape (k, d, d) containing the covariance
-               matrices for each cluster, initialized as identity matrices
+        X is a numpy.ndarray of shape (n, d)
+        k is a positive integer containing the number of clusters
+    Returns:
+        pi, m, S, or None, None, None
+            pi is a numpy.ndarray of shape (k,)
+            m is a numpy.ndarray of shape (k, d)
+            S is a numpy.ndarray of shape (k, d, d)
     """
-
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None
-    if not isinstance(k, int) or k < 1:
+
+    if not isinstance(k, int) or k <= 0:
         return None, None, None
 
     n, d = X.shape
 
-    # priors for each cluster, initialized evenly
-    phi = np.ones(k)/k
+    pi = np.full((k,), 1 / k)
 
-    # centroid means for each cluster, initialized with K-means
     m, _ = kmeans(X, k)
 
-    # covariance matrices for each cluster, initialized as identity matrices
-    S = np.tile(np.identity(d), (k, 1)).reshape(k, d, d)
+    S = np.tile(np.identity(d), (k, 1, 1))
 
-    return phi, m, S
+    return pi, m, S
