945b7387eb7da478f2225cc138a881cea3818633
Youssef <yussefboughanmy@gmail.com>
2025-08-02 21:35:11 +0100

    Clustering

--- unsupervised_learning/clustering/2-variance.py
+++ unsupervised_learning/clustering/2-variance.py
@@ -1,52 +1,34 @@
 
 #!/usr/bin/env python3
-"""Optimum K method aka inversed elbow"""
-
+"""Calculates total intra-cluster variance"""
 import numpy as np
-kmeans = __import__('1-kmeans').kmeans
-variance = __import__('2-variance').variance
 
 
-def optimum_k(X, kmin=1, kmax=None, iterations=1000):
+def variance(X, C):
     """
-    Optimun K method with variance
-    Args:
-        X: numpy.ndarray of shape (n, d) containing the data set
-        kmin: positive integer containing the minimum number of
-              clusters to check for (inclusive)
-        kmax: positive integer containing the maximum number of
-              clusters to check for (inclusive)
-        iterations: positive integer containing the maximum number
-                    of iterations for K-means
-    Returns: results, d_vars, or None, None on failure
-             results: list containing the outputs of K-means for
-                     each cluster size
-             d_vars: list containing the difference in variance from
-                     the smallest cluster size for each cluster size
-    """
-    try:
-        if not isinstance(X, np.ndarray) or len(X.shape) != 2:
-            return None, None
-        if not isinstance(iterations, int) or iterations < 1:
-            return None, None
-        if kmax is not None and (type(kmax) is not int or kmax < 1):
-            return None, None
-        if kmax is not None and kmin >= kmax:
-            return None, None
-        if kmax is None:
-            kmax = X.shape[0]
-        if not isinstance(kmin, int) or kmin < 1 or kmin >= X.shape[0]:
-            return None, None
+    Calculates the total intra-cluster variance for a dataset.
 
-        results = []
-        d_vars = []
-        for k in range(kmin, kmax + 1):
-            cluster, clss = kmeans(X, k, iterations)
-            results.append((cluster, clss))
-            variance_d = variance(X, cluster)
-            if k == kmin:
-                variance_k = variance_d
-            d_vars.append(variance_k - variance_d)
-        return results, d_vars
-    except Exception:
-        return None, None
+    Parameters:
+    - X (numpy.ndarray): 2D numpy array of shape `(n, d)` with the dataset.
+        - n is the number of data points
+        - d is the number of dimensions for each data point
+    - C (numpy.ndarray): 2D numpy array of shape `(k, d)` with the centroid
+        means for each cluster.
+        - k is the number of clusters
+        - d is the number of dimensions for each cluster centroid
+
+    Returns:
+    - float: The total variance, or `None` on failure.
+    """
+    if not isinstance(X, np.ndarray) or X.ndim != 2:
+        return None
+    if not isinstance(C, np.ndarray) or C.ndim != 2:
+        return None
+    if X.shape[1] != C.shape[1]:
+        return None
+
+    # Distances to closest centroid from each point
+    dists = np.min(np.linalg.norm(X[:, np.newaxis] - C, axis=-1), axis=-1)
+
+    # Variance of minimum distances to centroids (intra-cluster variance)
+    return np.sum(dists ** 2)
