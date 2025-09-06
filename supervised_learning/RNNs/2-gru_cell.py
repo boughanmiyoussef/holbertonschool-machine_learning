3d201fe5b553cbe728cd7890822c8f95f1442e17
Youssef <yussefboughanmy@gmail.com>
2025-09-06 16:57:45 +0100

    Commit

--- supervised_learning/RNNs/2-gru_cell.py
+++ supervised_learning/RNNs/2-gru_cell.py
@@ -1,66 +1,67 @@
 
 #!/usr/bin/env python3
+"""
+This module implements the GRUCell class, which represents a single unit of a
+Gated Recurrent Unit (GRU) network.
+"""
 
-"""Class that represents a cell of a simple RNN"""
 import numpy as np
 
 
-class GRUCell():
-    """Represents a cell of a simple RNN"""
+class GRUCell:
+    """
+    Represents a single unit of a Gated Recurrent Unit (GRU) network.
+    """
 
     def __init__(self, i, h, o):
-        """Constructor
-        @i: dimensionality of the data
-        @h: dimensionality of the hidden state
-        @o: dimensionality of the outputs
-        public instance attributes Wz, Wr, Wh, Wy, bz, br, bh, by
-        weights and biases
-            @Wz and bz are for the update gate
-            @Wr and br are for the reset gate
-            @Wh and bh are for the concatenated hidden state and input data
-            @wy and by are for the output
         """
-        self.Wz = np.random.normal(size=(h+i, h))
-        self.Wr = np.random.normal(size=(h+i, h))
-        self.Wh = np.random.normal(size=(h+i, h))
-        self.Wy = np.random.normal(size=(h, o))
+        Initializes the GRUCell with given dimensions for input, hidden state,
+        and output.
+        """
+        self.Wz = np.random.randn(i + h, h)
+        self.Wr = np.random.randn(i + h, h)
+        self.Wh = np.random.randn(i + h, h)
+        self.Wy = np.random.randn(h, o)
         self.bz = np.zeros((1, h))
         self.br = np.zeros((1, h))
         self.bh = np.zeros((1, h))
         self.by = np.zeros((1, o))
 
     def forward(self, h_prev, x_t):
-        """performs forward propagation for one time step
-        @h_prev: np.ndarray shape(m, h) containing the previous hidden state
-            @h_next: next hidden state
-            @y: output of the cell
-        @x_t: np.ndarray of shape(m, i) containing the data input for the cell
-            @m: batche size for the data
-        Retrun: h_next, y
-            @h_next is the next hidden state
-            @y: output of the cell
         """
-        con = np.concatenate((h_prev, x_t), axis=1)
+        Performs forward propagation for one time step.
+        """
+        concatenated = np.concatenate((h_prev, x_t), axis=1)
 
-        # update gate
-        x = np.matmul(con, self.Wz) + self.bz
-        # sigmoid
-        update = 1 / (1 + np.exp(-x))
+        # Update gate
+        z_t = self.sigmoid(np.dot(concatenated, self.Wz) + self.bz)
 
-        # reset gate
-        x = np.matmul(con, self.Wr) + self.br
-        # sigmoid
-        reset = 1 / (1 + np.exp(-x))
+        # Reset gate
+        r_t = self.sigmoid(np.dot(concatenated, self.Wr) + self.br)
 
-        con2 = np.concatenate((reset * h_prev, x_t), axis=1)
+        # Intermediate hidden state
+        concatenated_reset = np.concatenate((r_t * h_prev, x_t), axis=1)
+        h_tilde = np.tanh(np.dot(concatenated_reset, self.Wh) + self.bh)
 
-        tanh = np.tanh(np.matmul(con2, self.Wh) + self.bh)
-        # check equation - works for checker
-        h_next = update * tanh + (1 - update) * h_prev
+        # Next hidden state
+        h_next = (1 - z_t) * h_prev + z_t * h_tilde
 
-        soft = np.matmul(h_next, self.Wy) + self.by
-        # softmax
-        x_max = np.max(soft, axis=-1, keepdims=True)
-        x_exp = np.exp(soft - x_max)
-        y = x_exp / x_exp.sum(axis=-1, keepdims=True)
+        # Output using softmax activation
+        y = self.softmax(np.dot(h_next, self.Wy) + self.by)
+
         return h_next, y
+
+    @staticmethod
+    def sigmoid(x):
+        """
+        Applies the sigmoid activation function.
+        """
+        return 1 / (1 + np.exp(-x))
+
+    @staticmethod
+    def softmax(x):
+        """
+        Applies the softmax activation function.
+        """
+        exp_x = np.exp(x - np.max(x, axis=1, keepdims=True))
+        return exp_x / exp_x.sum(axis=1, keepdims=True)
