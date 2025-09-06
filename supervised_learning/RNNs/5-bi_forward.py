bdf1c2244978964d47b8dbef5dea64fa3531836c
Youssef <yussefboughanmy@gmail.com>
2025-09-06 17:03:36 +0100

    Commit

--- supervised_learning/RNNs/5-bi_forward.py
+++ supervised_learning/RNNs/5-bi_forward.py
@@ -1,41 +1,49 @@
-
 #!/usr/bin/env python3
-
-"""Class that represents a bidirectional cell of a RNN"""
+"""
+Bidirectional Cell Forward
+"""
 import numpy as np
 
 
-class BidirectionalCell():
-    """represents a bidirectional cell of a RNN"""
-
+class BidirectionalCell:
+    """
+    Represents a bidirectional cell of an RNN
+    """
     def __init__(self, i, h, o):
-        """Constructor
-        @i: dimensionality of the data
-        @h: dimensionality of the hidden state
-        @o: dimensionality of the outputs
-        public instance attributes Whf, Whb, Wy, bhf, bhb, by
-        weights and biases
-            @Whf and bhf are for the hidden states in the forward direction
-            @Whb and bhb are for the hidden states in the backward direction
-            @Wy and by are for the outputs
         """
-        self.Whf = np.random.normal(size=(h+i, h))
-        self.Whb = np.random.normal(size=(h+i, h))
-        self.Wy = np.random.normal(size=(2 * h, o))
-        self.bhb = np.zeros((1, h))
+        Class constructor
+        Args:
+            i is the dimensionality of the data
+            h is the dimensionality of the hidden states
+            o is the dimensionality of the outputs
+        Attributes:
+            Whf and bhf are for the hidden states in the forward direction
+            Whb and bhb are for the hidden states in the backward direction
+            Wy and by are for the outputs
+        """
+        self.Whf = np.random.randn(i + h, h)
         self.bhf = np.zeros((1, h))
+
+        self.Whb = np.random.randn(i + h, h)
+        self.bhb = np.zeros((1, h))
+
+        self.Wy = np.random.randn(2 * h, o)
         self.by = np.zeros((1, o))
 
     def forward(self, h_prev, x_t):
-        """performs forward propagation for one time step
-        @h_prev: np.ndarray shape(m, h) containing the previous hidden state
-        @x_t: np.ndarray of shape(m, i) containing the data input for the cell
-            @m: batche size for the data
-        Retrun: h_next
-            @h_next: the next hidden state
         """
-        con = np.concatenate((h_prev, x_t), axis=1)
+        Calculates the hidden state in the forward direction
+        Args:
+            x_t is a numpy.ndarray of shape (m, i)
+                m is the batch size for the data
+            h_prev is a numpy.ndarray of shape (m, h)
+        Returns:
+            h_next: the next hidden state
+        """
+        concat = np.concatenate((h_prev, x_t), axis=1)
 
-        h_next = np.tanh(np.matmul(con, self.Whf) + self.bhf)
+        h_next = np.tanh(
+            np.matmul(concat, self.Whf) + self.bhf
+        )
 
         return h_next
