1ed739316f9b5660fd77772ae3db89eebacfdc0b
Youssef <yussefboughanmy@gmail.com>
2025-09-06 16:56:49 +0100

    Commit

--- supervised_learning/RNNs/0-rnn_cell.py
+++ supervised_learning/RNNs/0-rnn_cell.py
@@ -1,40 +1,42 @@
 #!/usr/bin/env python3
-"""
-This module implements the RNNCell class for a simple Recurrent Neural Network.
-The class performs forward propagation through a single RNN cell.
-"""
 
+"""Class that represents a cell of a simple RNN"""
 import numpy as np
 
 
-class RNNCell:
-    """
-    Represents a cell of a simple Recurrent Neural Network (RNN).
-    """
+class RNNCell():
+    """Represents a cell of a simple RNN"""
 
     def __init__(self, i, h, o):
+        """Constructor
+        @i: dimensionality of the data
+        @h: dimensionality of the hidden state
+        @o: dimensionality of the outputs
+        public instance attributes Wh, Wy, bh, by - weights and biases
+            @Wh and bh are for the concatenated hidden state and input data
+            @wy and by are for the output
         """
-        Initialize the RNNCell with given dimensions for input, hidden state,
-        and output.
-        """
-        self.Wh = np.random.randn(i + h, h)
-        self.Wy = np.random.randn(h, o)
+        self.Wh = np.random.normal(size=(h+i, h))
+        self.Wy = np.random.normal(size=(h, o))
         self.bh = np.zeros((1, h))
         self.by = np.zeros((1, o))
 
     def forward(self, h_prev, x_t):
+        """performs forward propagation for one time step
+        @h_prev: np.ndarray shape(m, h) containing the previous hidden state
+            @h_next: next hidden state
+            @y: output of the cell
+        @x_t: np.ndarray of shape(m, i) containing the data input for the cell
+            @m: batche size for the data
+        Retrun: h_next, y
+            @h_next: the next hidden state
+            @y: output of the cell
         """
-        Performs forward propagation for one time step.
-        """
-        concatenated = np.concatenate((h_prev, x_t), axis=1)
-        h_next = np.tanh(np.dot(concatenated, self.Wh) + self.bh)
-        y = self.softmax(np.dot(h_next, self.Wy) + self.by)
+        con = np.concatenate((h_prev.T, x_t.T), axis=0)
+        h_next = np.tanh((np.matmul(con.T, self.Wh)) + self.bh)
+        soft = np.matmul(h_next, self.Wy) + self.by
+        # softmax
+        x_max = np.max(soft, axis=-1, keepdims=True)
+        x_exp = np.exp(soft - x_max)
+        y = x_exp / x_exp.sum(axis=-1, keepdims=True)
         return h_next, y
-
-    @staticmethod
-    def softmax(x):
-        """
-        Applies the softmax activation function.
-        """
-        exp_x = np.exp(x - np.max(x, axis=1, keepdims=True))
-        return exp_x / exp_x.sum(axis=1, keepdims=True)
--- supervised_learning/RNNs/1-rnn.py
+++ supervised_learning/RNNs/1-rnn.py
@@ -1,29 +1,36 @@
 #!/usr/bin/env python3
+"""
+This module implements the function rnn that performs forward propagation
+for a simple Recurrent Neural network (RNN) using an instance of the RNNCell
+class.
+"""
 
-"""Performs forward propagation for a simple RNN"""
 import numpy as np
 
 
 def rnn(rnn_cell, X, h_0):
-    """Performs forward propagation for simple RNN
-    @rnn_cell: instance of RNNCell that will be used for forward propagation
-    @X: data to be used, given as a np.ndarray shape(t, m, i)
-        @t: maxium number of time steps
-        @m: batch size
-        @i: dimensionality of the data
-    @h_0: initial hidden state given np.ndarray shape(m, h)
-        @h: dimensionality of the hidden state
-    Return: H, Y
-        @H: np.ndarray, all hidden states
-        @Y: np.ndarray, all of the outputs
     """
-    t = X.shape[0]
-    m, h = h_0.shape
+    Performs forward propagation for a simple RNN.
+    """
+    t, m, i = X.shape
+    _, h = h_0.shape
 
-    H = np.zeros((t+1, m, h))
-    Y = [0] * t
+    # Initialize the array to store hidden states
+    H = np.zeros((t + 1, m, h))
     H[0] = h_0
-    for j in range(t):
-        H[j+1], Y[j] = rnn_cell.forward(H[j], X[j])
 
-    return H, np.asarray(Y)
+    # Initialize a list to store the outputs
+    outputs = []
+
+    # Perform forward propagation through each time step
+    for step in range(t):
+        h_prev = H[step]
+        x_t = X[step]
+        h_next, y = rnn_cell.forward(h_prev, x_t)
+        H[step + 1] = h_next
+        outputs.append(y)
+
+    # Convert the list of outputs to a numpy array
+    Y = np.array(outputs)
+
+    return H, Y
