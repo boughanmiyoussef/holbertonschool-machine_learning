63c998addb1da4b61c58816c5f7d86f065001929
Youssef <yussefboughanmy@gmail.com>
2025-09-06 17:02:47 +0100

    Commit

--- supervised_learning/RNNs/4-deep_rnn.py
+++ supervised_learning/RNNs/4-deep_rnn.py
@@ -1,37 +1,48 @@
-
 #!/usr/bin/env python3
-
-"""Performs forward propagation for a deep RNN"""
+"""
+Performs forward propagation for a deep RNN
+"""
 import numpy as np
 
 
 def deep_rnn(rnn_cells, X, h_0):
-    """Performs forward propagation for deep RNN
-    @rnn_cell: list of RNNCell intances of length l that will be used
-    for forward propagation
-        @l: number of layers
-    @X: data to be used, given as a np.ndarray shape(t, m, i)
-        @t: maxium number of time steps
-        @m: batch size
-        @i: dimensionality of the data
-    @h_0: initial hidden state given np.ndarray shape(l, m, h)
-        @h: dimensionality of the hidden state
-    Return: H, Y
-        @H: np.ndarray, all hidden states
-        @Y: np.ndarray, all of the outputs
     """
-    t = X.shape[0]
-    l, m, h = h_0.shape
+    Performs forward propagation for a deep RNN
 
-    H = np.zeros((t+1, l, m, h))
-    Y = [0] * t
+    Args:
+        rnn_cells: list of RNNCell instances of length l
+        X: np.ndarray of shape (t, m, i) with input data
+        h_0: np.ndarray of shape (l, m, h) with initial hidden states
+
+    Returns:
+        H: np.ndarray of shape (t + 1, l, m, h) containing hidden states
+        Y: np.ndarray of shape (t, m, o) containing outputs
+    """
+    t, m, _ = X.shape
+    l, _, h = h_0.shape
+    o = rnn_cells[-1].by.shape[1]  # output dimension from the last RNNCell
+
+    # Initialize hidden states and outputs
+    H = np.zeros((t + 1, l, m, h))
     H[0] = h_0
+    Y = np.zeros((t, m, o))
+
     for step in range(t):
-        # get state at 0 to pass prev hidden state
-        h_state, output = rnn_cells[0].forward(H[step, 0], X[step])
-        H[step+1, 0, :, :] = h_state
-        for lay in range(1, l):
-            h_state, output = rnn_cells[lay].forward(H[step, lay], h_state)
-            H[step+1, lay, :, :] = h_state
-        Y[step] = output
-    return H, np.asarray(Y)
+        x_t = X[step]
+
+        for layer in range(l):
+            rnn_cell = rnn_cells[layer]
+            h_prev = H[step, layer]
+
+            if layer == 0:
+                x_in = x_t
+            else:
+                x_in = H[step + 1, layer - 1]
+
+            h_next, y = rnn_cell.forward(h_prev, x_in)
+            H[step + 1, layer] = h_next
+
+        # Use the output from the last layer only
+        Y[step] = y
+
+    return H, Y
