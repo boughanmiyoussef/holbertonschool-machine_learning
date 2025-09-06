0acc0b0f59663ed8a1076cf40b75265089e7f7b7
Youssef <yussefboughanmy@gmail.com>
2025-09-06 16:59:15 +0100

    Commit

--- supervised_learning/RNNs/2-gru_cell.py
+++ supervised_learning/RNNs/2-gru_cell.py
@@ -1,67 +1,95 @@
-
 #!/usr/bin/env python3
 """
-This module implements the GRUCell class, which represents a single unit of a
-Gated Recurrent Unit (GRU) network.
+Gated Recurrent Unit (GRU)
 """
-
 import numpy as np
 
 
 class GRUCell:
     """
-    Represents a single unit of a Gated Recurrent Unit (GRU) network.
+    Represents a gated recurrent unit
     """
-
     def __init__(self, i, h, o):
         """
-        Initializes the GRUCell with given dimensions for input, hidden state,
-        and output.
+        Class constructor
+        Args:
+            i is the dimensionality of the data
+            h is the dimensionality of the hidden state
+            o is the dimensionality of the outputs
+        The public instance attributes Wz, Wr, Wh, Wy, bz, br, bh, by
+            Wz and bz are for the update gate
+            Wr and br are for the reset gate
+            Wh and bh are for the intermediate hidden state
+            Wy and by are for the output
         """
         self.Wz = np.random.randn(i + h, h)
-        self.Wr = np.random.randn(i + h, h)
-        self.Wh = np.random.randn(i + h, h)
-        self.Wy = np.random.randn(h, o)
         self.bz = np.zeros((1, h))
+
+        self.Wr = np.random.randn(i + h, h)
         self.br = np.zeros((1, h))
+
+        self.Wh = np.random.randn(i + h, h)
         self.bh = np.zeros((1, h))
+
+        self.Wy = np.random.randn(h, o)
         self.by = np.zeros((1, o))
 
     def forward(self, h_prev, x_t):
         """
-        Performs forward propagation for one time step.
+        Forward propagation for one time step
+        Argrs:
+            h_prev is the previous hidden state
+            x_t is the input data for the cell
+        Returns:
+            h_next is the next hidden state
+            y_t is the output of the cell
         """
-        concatenated = np.concatenate((h_prev, x_t), axis=1)
+        # Concatenate previous hidden state and current input
+        concat_h_x = np.concatenate((h_prev, x_t), axis=1)
 
         # Update gate
-        z_t = self.sigmoid(np.dot(concatenated, self.Wz) + self.bz)
+        z_t = self.sigmoid(np.dot(concat_h_x, self.Wz) + self.bz)
 
         # Reset gate
-        r_t = self.sigmoid(np.dot(concatenated, self.Wr) + self.br)
+        r_t = self.sigmoid(np.dot(concat_h_x, self.Wr) + self.br)
 
         # Intermediate hidden state
-        concatenated_reset = np.concatenate((r_t * h_prev, x_t), axis=1)
-        h_tilde = np.tanh(np.dot(concatenated_reset, self.Wh) + self.bh)
+        concat_r_h_x = np.concatenate((h_prev * r_t, x_t), axis=1)
 
-        # Next hidden state
-        h_next = (1 - z_t) * h_prev + z_t * h_tilde
+        # h_hat_t
+        h_hat_t = np.tanh(np.dot(concat_r_h_x, self.Wh) + self.bh)
 
-        # Output using softmax activation
-        y = self.softmax(np.dot(h_next, self.Wy) + self.by)
+        # Final hidden state
+        h_next = (1 - z_t) * h_prev + z_t * h_hat_t
 
+        # Output
+        y_t = np.dot(h_next, self.Wy) + self.by
+
+        # Softmax activation
+        y = self.softmax(y_t)
+
+        # Return the next hidden state and the output
         return h_next, y
 
     @staticmethod
     def sigmoid(x):
         """
-        Applies the sigmoid activation function.
+        Sigmoid activation function
+        Args:
+            x is the input data
+        Returns:
+            The sigmoid of x
         """
         return 1 / (1 + np.exp(-x))
 
     @staticmethod
     def softmax(x):
         """
-        Applies the softmax activation function.
+        Softmax activation function
+        Args:
+            x is the input data
+        Returns:
+            The softmax of x
         """
-        exp_x = np.exp(x - np.max(x, axis=1, keepdims=True))
+        exp_x = np.exp(x - np.max(x))
         return exp_x / exp_x.sum(axis=1, keepdims=True)
