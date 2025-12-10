9acf47e10d17f0463153680ee2625abf02927355
Youssef <yussefboughanmy@gmail.com>
2025-12-10 20:41:36 +0100

    crop of an image

--- pipeline/data_augmentation/1-crop.py
+++ pipeline/data_augmentation/1-crop.py
@@ -1,44 +1,23 @@
 #!/usr/bin/env python3
+
+"""script for
 """
-Performs a random crop of an image
-"""
+
 import tensorflow as tf
 
 
 def crop_image(image, size):
+    """[Function that flips an image horizontally]
+
+    Args:
+        image ([ 3D tf.Tensor]): [tf.Tensor containing the image to flip]
+        size is a tuple containing the size of the crop
+    Returns
+        the flipped image
     """
-    Performs a random crop of an image
-    :param image: is a 3D tf.Tensor containing the image to crop
-    :param size: is a tuple containing the size of the crop
-    :return: the cropped image
-    """
-    # Unpack size
-    crop_height, crop_width, channels = size
-    
-    # Get image shape
-    image_shape = tf.shape(image)
-    img_height, img_width = image_shape[0], image_shape[1]
-    
-    # Calculate maximum offset
-    max_offset_height = tf.maximum(0, img_height - crop_height)
-    max_offset_width = tf.maximum(0, img_width - crop_width)
-    
-    # Generate random offsets
-    offset_height = tf.random.uniform(
-        shape=[], minval=0, maxval=max_offset_height + 1, dtype=tf.int32)
-    offset_width = tf.random.uniform(
-        shape=[], minval=0, maxval=max_offset_width + 1, dtype=tf.int32)
-    
-    # Perform crop
-    cropped = image[
-        offset_height:offset_height + crop_height,
-        offset_width:offset_width + crop_width,
-        :
-    ]
-    
-    # If crop dimensions don't match requested (edge case), resize
-    cropped_shape = tf.shape(cropped)
-    if crop_height != cropped_shape[0] or crop_width != cropped_shape[1]:
-        cropped = tf.image.resize(cropped, (crop_height, crop_width))
-    
-    return cropped
+
+    # TensorFlow. 'x' = A placeholder for an image.
+
+    cropped_image = tf.image.random_crop(image, size)
+
+    return cropped_image
