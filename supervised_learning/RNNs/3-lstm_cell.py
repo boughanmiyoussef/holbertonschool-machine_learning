81ac4d46c447dc0ff8942a006e2b700797fa6622
Youssef Boughanmi <147752524+boughanmiyoussef@users.noreply.github.com>
2026-03-17 18:22:27 +0100

    Refactor LSTMCell implementation
    Refactor LSTMCell class for improved clarity and functionality.

--- supervised_learning/RNNs/3-lstm_cell.py
+++ supervised_learning/RNNs/3-lstm_cell.py
@@ -1,71 +1,70 @@
 #!/usr/bin/env python3
+""" LSTM Cell implementation in Python """
 
-"""Class that represents an LSTM unit"""
 import numpy as np
 
 
-
-class LSTMCell():
-    """epresents an LSTM unit"""
+class LSTMCell:
+    """Represents an LSTM unit."""
 
     def __init__(self, i, h, o):
-        """Constructor
-        @i: dimensionality of the data
-        @h: dimensionality of the hidden state
-        @o: dimensionality of the outputs
-        public instance attributes Wf, Wu, Wc, Wo, Wy, bf, bu, bc, bo, by
-        weights and biases
-            @Wf and bf are for the forget gate
-            @Wu and bu are for the update gate
-            @Wc and bc are for the intermediate cell state
-            @Wo and bo are for the output gate
-            @Wy and by are for the outputs
         """
-        self.Wf = np.random.normal(size=(h+i, h))
-        self.Wu = np.random.normal(size=(h+i, h))
-        self.Wc = np.random.normal(size=(h+i, h))
-        self.Wo = np.random.normal(size=(h+i, h))
-        self.Wy = np.random.normal(size=(h, o))
-        self.bo = np.zeros((1, h))
-        self.bc = np.zeros((1, h))
-        self.bu = np.zeros((1, h))
+        Constructor for the LSTMCell class.
+        """
+        self.Wf = np.random.randn(i + h, h)
+        self.Wu = np.random.randn(i + h, h)
+        self.Wc = np.random.randn(i + h, h)
+        self.Wo = np.random.randn(i + h, h)
+        self.Wy = np.random.randn(h, o)
+
         self.bf = np.zeros((1, h))
+        self.bu = np.zeros((1, h))
+        self.bc = np.zeros((1, h))
+        self.bo = np.zeros((1, h))
         self.by = np.zeros((1, o))
 
-    def forward(self, h_prev, c_prev, x_t):
-        """performs forward propagation for one time step
-        @h_prev: np.ndarray shape(m, h) containing the previous hidden state
-        @x_t: np.ndarray of shape(m, i) containing the data input for the cell
-            @m: batche size for the data
-        Retrun: h_next, c_next, y
-            @h_next is the next hidden state
-            @y: output of the cell
+    def sigmoid(self, z):
         """
-        con = np.concatenate((h_prev, x_t), axis=1)
+        Sigmoid activation function.
+        """
+        return 1 / (1 + np.exp(-z))
 
-        # forget gate
-        x = np.matmul(con, self.Wf) + self.bf
-        # sigmoid
-        forget = 1 / (1 + np.exp(-x))
+    def softmax(self, z):
+        """
+        Softmax activation function.
+        """
+        exp_z = np.exp(z - np.max(z, axis=1, keepdims=True))
+        return exp_z / np.sum(exp_z, axis=1, keepdims=True)
 
-        # update gate
-        x = np.matmul(con, self.Wu) + self.bu
-        # sigmoid
-        update = 1 / (1 + np.exp(-x))
+    def forward(self, h_prev, c_prev, x_t):
+        """
+        Forward propagation for one time step.
+        """
+        m, i = x_t.shape
+        h = h_prev.shape[1]
 
-        # output gate
-        x = np.matmul(con, self.Wo) + self.bo
-        # sigmoid
-        out = 1 / (1 + np.exp(-x))
+        # Concatenate x_t and h_prev to form input for gates
+        concat_x_h = np.concatenate((h_prev, x_t), axis=1)
 
-        c = np.tanh(np.matmul(con, self.Wc) + self.bc)
+        # Forget gate
+        ft = self.sigmoid(np.dot(concat_x_h, self.Wf) + self.bf)
 
-        c_next = forget * c_prev + c * update
-        h_next = out * np.tanh(c_next)
+        # Update gate
+        ut = self.sigmoid(np.dot(concat_x_h, self.Wu) + self.bu)
 
-        soft = np.matmul(h_next, self.Wy) + self.by
-        # softmax
-        x_max = np.max(soft, axis=-1, keepdims=True)
-        x_exp = np.exp(soft - x_max)
-        y = x_exp / x_exp.sum(axis=-1, keepdims=True)
+        # Intermediate cell state
+        c_hat = np.tanh(np.dot(concat_x_h, self.Wc) + self.bc)
+
+        # Next cell state
+        c_next = ft * c_prev + ut * c_hat
+
+        # Output gate
+        ot = self.sigmoid(np.dot(concat_x_h, self.Wo) + self.bo)
+
+        # Next hidden state
+        h_next = ot * np.tanh(c_next)
+
+        # Output
+        y = self.softmax(np.dot(h_next, self.Wy) + self.by)
+
         return h_next, c_next, y
