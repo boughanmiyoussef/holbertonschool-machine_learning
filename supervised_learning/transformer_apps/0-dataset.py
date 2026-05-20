7e827d0e867062084eb04e7f1ac18b3c92312ddc
Youssef Boughanmi <147752524+boughanmiyoussef@users.noreply.github.com>
2026-05-20 07:33:12 +0100

    Refactor Dataset class and improve docstrings

--- supervised_learning/transformer_apps/0-dataset.py
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -1,44 +1,47 @@
 #!/usr/bin/env python3
 """
-Create the class Dataset that loads and preps a dataset for machine translation
+0-dataset.py
 """
 import tensorflow.compat.v2 as tf
 import tensorflow_datasets as tfds
 
 
 class Dataset:
-    """
-    Loads and preps a dataset
-    """
+    """class that loads and preps a dataset for machine translation"""
 
     def __init__(self):
-        """
-        Class constructor
-        """
+        """constructor"""
+
+        # Portugese-English translation dataset
+        # from the TED Talks Open Translation Project
+        # contains approximately 50000 training examples,
+        # 1100 validation examples, and 2000 test examples
+
+        # Download the dataset
         examples, metadata = tfds.load('ted_hrlr_translate/pt_to_en',
                                        with_info=True,
                                        as_supervised=True)
-        self.data_train, self.data_valid = (examples['train'],
-                                            examples['validation'])
+        # print(type(examples))
+        # print(examples)
+        # print(examples['train'])
 
+        # Extract the train and validation datasets
+        self.data_train = examples['train']
+        self.data_valid = examples['validation']
+
+        # Create sub-word tokenizers for the dataset
         self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
             self.data_train)
 
     def tokenize_dataset(self, data):
-        """
-        Creates sub-word tokenizers for our dataset
-        :param data: a tf.data.Dataset whose examples are formatted as a
-        tuple (pt, en)
-            pt is the tf.Tensor containing the Portuguese sentence
-            en is the tf.Tensor containing the corresponding English sentence
-        :return: tokenizer_pt, tokenizer_en
-            tokenizer_pt is the Portuguese tokenizer
-            tokenizer_en is the English tokenizer
-        """
-        tokenizer_pt = tfds.features.text.SubwordTextEncoder.build_from_corpus(
-            (pt.numpy() for pt, en in data), target_vocab_size=2 ** 15)
+        """function that creates sub-word tokenizers for a dataset"""
 
         tokenizer_en = tfds.features.text.SubwordTextEncoder.build_from_corpus(
-            (en.numpy() for pt, en in data), target_vocab_size=2 ** 15)
+            (en.numpy() for pt, en in data),
+            target_vocab_size=2**15)
+
+        tokenizer_pt = tfds.features.text.SubwordTextEncoder.build_from_corpus(
+            (pt.numpy() for pt, en in data),
+            target_vocab_size=2**15)
 
         return tokenizer_pt, tokenizer_en
